e62f1520be2f66231141068c4a97c50a8d22f205
Misho's Mac <mishosmac@Misha-2.local>
2024-11-13 14:57:43 +0400

    initial commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Misho's Mac on 13.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
